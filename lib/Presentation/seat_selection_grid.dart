d3f2f76b438c69a648840fe45104fe69d054c198
jame12945 <jame12945@gmail.com>
2023-12-12 23:45:08 +0700

    update lastest

--- lib/Presentation/seat_selection_grid.dart
+++ lib/Presentation/seat_selection_grid.dart
@@ -30,34 +30,34 @@ class _SeatSelectionGridState extends State<SeatSelectionGrid> {
           child: Stack(
             children: [
               Transform.translate(
-                offset:Offset(330,40),
+                offset:Offset(330,50),
                 child: Text(
                   'เลือกที่นั่ง',
                   style: TextStyle(
                       fontSize: 40,
-                      fontWeight: FontWeight.w700
+                      fontWeight: FontWeight.w600
                   ),
                 ),
               ),
               Transform.translate(
-                offset:Offset(0,120),
+                offset:Offset(0,150),
                 child: Container(
                   height: 2,
-                  color: Colors.grey,
+                  color: Colors.black12,
                 ),
               ),
               Transform.translate(
-                offset:Offset(140,160),
+                offset:Offset(120,200),
                 child: Text(
-                  '1           2          3         4         5',
+                  '1         2       3        4        5',
                   style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.w700
+                      fontSize: 28,
+                      fontWeight: FontWeight.w800
                   ),
                 ),
               ),
               Transform.translate(
-                offset: Offset(30, 280),
+                offset: Offset(40, 270),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -65,39 +65,39 @@ class _SeatSelectionGridState extends State<SeatSelectionGrid> {
                     Text(
                       'A',
                       style: TextStyle(
-                        fontSize: 40,
+                        fontSize: 28,
                         fontWeight: FontWeight.w700,
                       ),
                     ),
-                    SizedBox(height: 70),
+                    SizedBox(height: 32),
                     Text(
                       'B',
                       style: TextStyle(
-                        fontSize: 40,
+                        fontSize: 28,
                         fontWeight: FontWeight.w700,
                       ),
                     ),
-                    SizedBox(height: 70),
+                    SizedBox(height: 34),
                     Text(
                       'C',
                       style: TextStyle(
-                        fontSize: 40,
+                        fontSize: 28,
                         fontWeight: FontWeight.w700,
                       ),
                     ),
-                    SizedBox(height: 60),
+                    SizedBox(height: 34),
                     Text(
                       'D',
                       style: TextStyle(
-                        fontSize: 40,
+                        fontSize: 28,
                         fontWeight: FontWeight.w700,
                       ),
                     ),
-                    SizedBox(height: 60),
+                    SizedBox(height: 40),
                     Text(
                       'E',
                       style: TextStyle(
-                        fontSize: 40,
+                        fontSize: 28,
                         fontWeight: FontWeight.w700,
                       ),
                     ),
@@ -108,13 +108,13 @@ class _SeatSelectionGridState extends State<SeatSelectionGrid> {
               Transform.translate(
                 offset: Offset(100,90.0),
                 child: Container(
-                  width: 600,
+                  width: 360,
                   child: Transform.translate(
                     offset: Offset(0, 80),
                     child: GridView.builder(
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 5,
-                        crossAxisSpacing: 8,
+                        crossAxisSpacing: 6,
                         mainAxisSpacing: 8,
                       ),
                       itemCount: 25,
@@ -135,11 +135,10 @@ class _SeatSelectionGridState extends State<SeatSelectionGrid> {
                           child: Transform.translate(
                             offset: Offset(0, 60*1 ),
                             child: Container(
-                              width: 40.0,
-                              height: 40.0,
+
                               decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20.0),
-                                color: seat.status == 'available' ? Colors.grey : Colors.orange,
+                                borderRadius: BorderRadius.circular(10.0),
+                                color: seat.status == 'available' ? Colors.black26 : Colors.orange,
                               ),
                             ),
                           ),
@@ -151,24 +150,25 @@ class _SeatSelectionGridState extends State<SeatSelectionGrid> {
                 ),
               ),
               Transform.translate(
-                offset:Offset(0,950),
+                offset:Offset(0,1050),
                 child: Container(
                   height: 2,
-                  color: Colors.grey,
+                  color: Colors.black12,
                 ),
               ),
               Transform.translate(
-                offset:Offset(20,1000),
+                offset:Offset(32,1080),
                 child: Text(
                   'ที่นั่ง',
                   style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w700
+                      fontSize: 26,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black54
                   ),
                 ),
               ),
               Transform.translate(
-                  offset: Offset(10, 1050),
+                  offset: Offset(10, 1130),
                   child: buildSelectedSeatsDetails()),
             ],
 
@@ -198,26 +198,33 @@ class _SeatSelectionGridState extends State<SeatSelectionGrid> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
 
-          SizedBox(height: 8.0),
           for (String seatNumber in selectedSeats)
-            ElevatedButton(
-              onPressed: () {
-                // Handle cancel button pressed
-                cancelSelectedSeat(seatNumber);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black, // สีพื้นหลังของปุ่ม
-                foregroundColor: Colors.white, // สีของข้อความ
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)
+            Transform.translate(
+              offset: Offset(12, 0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle cancel button pressed
+                  cancelSelectedSeat(seatNumber);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0)
+                  ),
+
                 ),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(seatNumber+' ',style: TextStyle(fontSize: 20),),
-                  Icon(Icons.clear),
-                ],
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(seatNumber,style: TextStyle(fontSize: 20),),
+
+                    Transform.translate(
+                        offset: Offset(10, 0),
+                        child: Icon(Icons.clear)
+                    ),
+                  ],
+                ),
               ),
             ),
         ],
